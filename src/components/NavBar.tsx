9c0c8cb6ba48a6fd9260b28f1b36e5db2953b3cf
Nic3holas-wq <nicholasmurimi254@gmail.com>
2025-07-04 20:24:37 +0300

    feat(cart): add framer-motion animation to cart item badge

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,17 @@
-// src/components/Navbar.tsx
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { HiOutlineShoppingCart, HiOutlineUser, HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
 import { motion, AnimatePresence } from 'framer-motion';
 import SearchBar from './SearchBar';
+import { useCart } from "../context/CartContext";
 
 const Navbar: React.FC = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const { cart } = useCart(); // ✅ Destructure 'cart' from context
+
+    const totalQuantity = cart.reduce((acc, item) => acc + item.quantity, 0);
+
+
   return (
     <nav className="bg-white fixed top-0 left-0 right-0 z-50 shadow">
       {/* Top Navbar */}
@@ -24,13 +29,26 @@ const Navbar: React.FC = () => {
             <HiOutlineUser className='text-gray-500' size={28} />
           </Link>
           <Link to="/cart" className="relative flex items-center gap-2 hover:text-indigo-600 transition">
-            <HiOutlineShoppingCart className='text-gray-500' size={28} />
+  <HiOutlineShoppingCart className='text-gray-500' size={28} />
 
-            {/* Badge */}
-            <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
-                3
-            </span>
-          </Link>
+  <AnimatePresence>
+  {totalQuantity > 0 && (
+    <motion.span
+      key={`cart-count-${totalQuantity}`}
+      initial={{ y: -20, opacity: 0 }}
+animate={{ y: 0, opacity: 1 }}
+exit={{ y: -20, opacity: 0 }}
+transition={{ type: "spring", stiffness: 300, damping: 10 }}
+
+      className="absolute -top-2 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full"
+    >
+      {totalQuantity}
+    </motion.span>
+  )}
+</AnimatePresence>
+
+</Link>
+
 
         </div>
       </div>
@@ -43,7 +61,7 @@ const Navbar: React.FC = () => {
             animate={{ x: 0 }}
             exit={{ x: '-100%' }}
             transition={{ duration: 0.6 }}
-            className="fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-50 p-6"
+            className="fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-1000 p-6"
           >
             <button onClick={() => setIsOpen(false)} className="mb-4">
               <HiOutlineX size={28} className="text-gray-700" />
